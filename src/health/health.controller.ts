f5fd7823dd28c7f0a88cbb5333bfd945e6124d59
superChokkk <vcestaemchok@gmail.com>
2025-12-10 20:16:54 -0300

    build image gateway

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,21 +3,10 @@ import { HealthCheck, HealthCheckService, HttpHealthIndicator, TypeOrmHealthIndi
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private db: TypeOrmHealthIndicator,
-    
-  ) {}
-
+  
   @Get()
-  @HealthCheck()
   check() {
-    // Você pode adicionar outras verificações aqui, como a conexão com o banco de dados.
-    // Por exemplo: () => this.db.pingCheck('database')
-    return this.health.check([
-      //() => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
-      () => this.db.pingCheck('database', { timeout: 300 }),
-    ]);
+    // Resposta simples, igual aos microsserviços
+    return { status: 'ok', service: 'gateway' };
   }
 }
