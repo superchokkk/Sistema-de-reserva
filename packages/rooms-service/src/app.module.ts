2172c1264cfb8e764a89265f7c2685775c2d01fd
superChokkk <vcestaemchok@gmail.com>
2025-12-11 10:33:30 -0300

    new imagens

--- packages/rooms-service/src/app.module.ts
+++ packages/rooms-service/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { RoomsModule } from './rooms/rooms.module';
-
+//coment
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
